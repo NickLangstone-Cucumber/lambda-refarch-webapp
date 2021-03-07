a2e26e330e4c722e7e1d21b8d4c6cf753bee5196
Nick <nick.langstone@cucumber.co.nz>
2021-03-08 09:41:21 +1300

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "",     // CognitoClientID
-  "api_base_url": "",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "",                   // CognitoDomainName
-  "redirect_url": ""                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "4g24rrukuld8l0atnh1e5jivvt",     // CognitoClientID
+  "api_base_url": "https://xitdxh1201.execute-api.ap-southeast-2.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "springsheep-springsheep.auth.ap-southeast-2.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d3q68ieyfufh9e.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
